45f9f7d3f138a314316f8fe1b66a5104f6aec380
Kostyazh <zhebelev.konstantin@yandex.ua>
2020-10-24 14:51:52 +0300

    js validation

--- index.js
+++ index.js
@@ -1,2 +1,59 @@
-document.addEventListener('DOMContentLoaded', function(){
+ document.addEventListener('DOMContentLoaded', function(){
+   const form = document.getElementById('form');
+   form.addEventListener('submit', formSend);
+
+   async function formSend (e) {
+e.preventDefault();
+
+let error = formValidate(form);
+
+if(error === 0){
+
+}else{
+  alert('Заполните обязательные поля');
+}
+   }
+
+   function formValidate(form){
+     let error = 0;
+     let formReq = document.querySelectorAll('._req');
+
+     for (let index = 0; index < formReq.length; index++){
+       const input = formReq[index];
+       formRemoveError(input);
+
+
+       if(input.classList.contains('_email')){
+         if(emailTest(input)){
+           formAddError(input);
+           error++;
+         }
+       }else if (input.getAttribute('type') === 'checkbox' && input.checked === false ) {
+         formAddError(input);
+         error++;
+       }else{
+         if(input.value === ''){
+           formAddError(input);
+           error++;
+         }
+       }
+     }
+     return error;
+   }
+
+   function formAddError(input){
+     input.parentElement.classList.add('_error');
+     input.classList.add('_error');
+   }
+
+   function formRemoveError(input){
+     input.parentElement.classList.remove('_error');
+     input.classList.remove('_error');
+   }
+
+   function emailTest(input){
+    let re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+   }
+
 });
